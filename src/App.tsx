2fca23124f00fcd3e601bc494679edcd439f1db2
Daniele Baelde <Sig.Baldi@gmail.com>
2021-12-12 21:40:56 +0000

    Initial style

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <h1>Bookmarks</h1>
-        <button className="add-link-button" data-testid="addLinkButton">
+        <button data-testid="addLinkButton">
           +
         </button>
       </header>
